2308bed5e6c07b6e0be7e68baeb8c7d110f0841d
백승의 <baekqcdo0902@gmail.com>
2026-07-31 17:31:29 +0900

    3일차 마무리

--- js/script.js
+++ js/script.js
@@ -67,6 +67,30 @@ $(function () {
         if (uv < 11) return "매우 높음";
         return "위험";
     }
+    
+    const FALLBACK_CITIES = {
+        '서울': { lat: 37.5665, lon: 126.9780 },
+        '부산': { lat: 35.1796, lon: 129.0756 },
+        '대구': { lat: 35.8714, lon: 128.6014 },
+        '인천': { lat: 37.4563, lon: 126.7052 },
+        '광주': { lat: 35.1595, lon: 126.8526 },
+        '대전': { lat: 36.3504, lon: 127.3845 },
+        '울산': { lat: 35.5384, lon: 129.3114 },
+        '세종': { lat: 36.4800, lon: 127.2890 },
+        '수원': { lat: 37.2636, lon: 127.0286 },
+        '고양': { lat: 37.6584, lon: 126.8320 },
+        '용인': { lat: 37.2411, lon: 127.1776 },
+        '성남': { lat: 37.4449, lon: 127.1388 },
+        '청주': { lat: 36.6424, lon: 127.4890 },
+        '전주': { lat: 35.8242, lon: 127.1480 },
+        '천안': { lat: 36.8151, lon: 127.1139 },
+        '포항': { lat: 36.0190, lon: 129.3435 },
+        '창원': { lat: 35.2281, lon: 128.6811 },
+        '제주': { lat: 33.4996, lon: 126.5312 },
+        '춘천': { lat: 37.8813, lon: 127.7298 },
+        '강릉': { lat: 37.7519, lon: 128.8761 }
+    };
+    const HOME_CITIES = ['서울', '부산', '대구', '인천', '광주', '대전', '제주', '강릉'];
     // ----------------------------------------
     // 화면 전환: 홈 ↔ 상세
     // ----------------------------------------
@@ -129,6 +153,27 @@ $(function () {
                 showError("날씨 정보를 가져오지 못했습니다. 잠시후 다시 시도해주세요.");
             })
     }
+
+    function loadCityList() {
+        //HOME_CITIES 내의 도시들의 위도를 하나의 문자열로 연결
+        const lats = HOME_CITIES.map(function(name) {return FALLBACK_CITIES[name].lat;}).join(",");
+        //HOME_CITIES 내의 도시들의 경도를 하나의 문자열로 연결
+        const lons = HOME_CITIES.map(function(name) {return FALLBACK_CITIES[name].lon;}).join(",");
+
+        $.getJSON("https://api.open-meteo.com/v1/forecast", {
+            latitude: lats,
+            longitude: lons,
+            current: "temperature_2m,weather_code",
+            daily: "temperature_2m_max,temperature_2m_min",
+            forecast_days: 1,
+            timezone:"auto"
+        })        
+        .done(function(results) { alert("주요 도시 날씨들을 불러습니다!"); })
+        .fail(function() { alert("주요 도시 날씨를 불러오지 못했습니다.")});
+    }
+    loadCityList();
+
+
 
     // ----------------------------------------
     // 받아온 데이터 화면에 표시
@@ -233,6 +278,7 @@ $(function () {
         $("#hourlyList").html(rows).find(".hour-detail").hide();
         // .find(".hourly-row").hide();
     }
+    // function cityRowHTML(name,)
     // ----------------------------------------
     // 데이터 로딩 완료
     //  - 상태 메세지 숨기기
